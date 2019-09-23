8730dd539dc50c690c06a9d80970e0f8253d73fe
alamminsalo <antti.lamminsalo@protonmail.com>
2019-09-23 07:01:35 +0300

    impl for handlebars templates

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,14 @@ use handlebars::Handlebars;
 
 fn gen_models_oa3(template_path: &Path, lang: Lang, components: Components) {
     let mut hb = Handlebars::new();
-    // hb.register_template_file("model", template_path).unwrap();
+    hb.register_template_file("model", template_path).unwrap();
     let template = mustache::compile_path(template_path).unwrap();
     for (key, schema) in components.schemas.unwrap().into_iter() {
         match schema {
             Object(s) => {
                 let model = Model::new(key, s, &lang);
-                // let render = hb.render("model", &model).unwrap();
-                let render = template.render_to_string(&model).unwrap();
+                let render = hb.render("model", &model).unwrap();
+                // let render = template.render_to_string(&model).unwrap();
                 // decode special characters
                 let decoded = htmlescape::decode_html(&render).unwrap();
                 println!("{}", &decoded);
