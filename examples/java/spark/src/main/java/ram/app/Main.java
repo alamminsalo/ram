ce01058a07c840b428e002b9e097287541ca87a7
alamminsalo <antti.lamminsalo@bitcomp.fi>
2020-01-10 09:51:14 +0200

    add spark example

---
+++ examples/java/spark/src/main/java/ram/app/Main.java
@@ -0,0 +1,10 @@
+package ram.app;
+
+import ram.generated.model.*;
+import ram.generated.controller.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("COMPILED OK!");
+    }
+}
