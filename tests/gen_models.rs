90dbdaa86bd074e0a92a022655f68c1eaa68c712
alamminsalo <antti.lamminsalo@bitcomp.fi>
2020-01-26 13:19:43 +0200

    improved tests: now tests generated files based on model properties

--- tests/gen_models.rs
+++ tests/gen_models.rs
@@ -1,3 +1,4 @@
+use inflector::Inflector;
 use ram::Config;
 use regex::Regex;
 use std::collections::HashMap;
@@ -34,12 +35,13 @@ fn it_generates_models_rust() {
 
     // assert vars
     let models_count = 7;
+    let mut models = vec![];
 
     match spec {
         openapi::OpenApi::V3_0(spec) => {
-            let models = ram::generate_models_v3(&spec, &specpath);
+            models = ram::generate_models_v3(&spec, &specpath);
             assert_eq!(models.len(), models_count);
-            ram::generate_files(cfg, models, vec![], &output)
+            ram::generate_files(cfg, models.clone(), vec![], &output)
         }
         _ => {}
     };
@@ -57,10 +59,93 @@ fn it_generates_models_rust() {
     // assert files count: models + mod file
     assert_eq!(files.len(), models_count + 1);
 
-    // do some regex checking
-    // check that "farm.rs" contains 9 occurences of 'pub'
-    let re = Regex::new(r"pub").unwrap();
-    let contents: String = std::fs::read_to_string(files.get("farm.rs").unwrap().path()).unwrap();
+    // gather some variables from models
+    for model in models {
+        dbg!(&model);
 
-    assert_eq!(re.find_iter(&contents).count(), 9);
+        let props_iter = model.properties.iter().chain(
+            model
+                .additional_properties
+                .iter()
+                .flat_map(|p| &p.properties),
+        );
+
+        // pub should occur in field names and struct def
+        let count_pub = props_iter.clone().count() + 1;
+        let count_i32 = props_iter
+            .clone()
+            .filter(|p| {
+                p.schema_type == "integer"
+                    || (p.schema_type == "array"
+                        && match p.items.as_ref() {
+                            Some(item) => item.schema_type == "integer",
+                            _ => false,
+                        })
+            })
+            .count();
+        let count_option = props_iter
+            .clone()
+            .filter(|p| {
+                p.nullable
+                    || (p.schema_type == "array"
+                        && match p.items.as_ref() {
+                            Some(item) => item.nullable,
+                            _ => false,
+                        })
+            })
+            .count();
+        let count_box = props_iter
+            .clone()
+            .filter(|p| {
+                p.schema_type == "object"
+                    || (p.schema_type == "array"
+                        && match p.items.as_ref() {
+                            Some(item) => item.schema_type == "object",
+                            _ => false,
+                        })
+            })
+            .count();
+        let count_vec = props_iter
+            .clone()
+            .filter(|p| p.schema_type == "array")
+            .count();
+
+        println!("counted test variables!");
+
+        // do some regex checking
+        // check that "model.rs" contains 9 occurences of 'pub'
+        let contents: String = std::fs::read_to_string(
+            files
+                .get(&format!("{}.rs", &model.name.to_snake_case()))
+                .unwrap()
+                .path(),
+        )
+        .unwrap();
+
+        println!("read file contents");
+
+        assert_eq!(
+            Regex::new(r"pub").unwrap().find_iter(&contents).count(),
+            count_pub
+        );
+        assert_eq!(
+            Regex::new(r"i32").unwrap().find_iter(&contents).count(),
+            count_i32
+        );
+        assert_eq!(
+            Regex::new(r"Box<.+>").unwrap().find_iter(&contents).count(),
+            count_box
+        );
+        assert_eq!(
+            Regex::new(r"Vec<.+>").unwrap().find_iter(&contents).count(),
+            count_vec
+        );
+        assert_eq!(
+            Regex::new(r"Option<.+>")
+                .unwrap()
+                .find_iter(&contents)
+                .count(),
+            count_option
+        );
+    }
 }
